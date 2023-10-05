35af309e831c10c3a46710b015a76977f3799ecf
Nikita Chebanenko <110260850+nY2en@users.noreply.github.com>
2023-10-05 21:40:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/phonebook">
       <ChakraProvider theme={theme}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
